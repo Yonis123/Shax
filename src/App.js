670e00841cef1c6084894184185d4db258e54153
Yonis123 <ynur472@gmail.com>
2024-07-18 14:04:45 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import WoodenBox from './components/WoodenBox';
 function App() {
   return (
     <div >
-      <h1>Hi</h1>
+      
       {<Shaxboard></Shaxboard> }
       {/* <WoodenBox></WoodenBox> */}
     </div>
