5b205a0813e597feda47750a03ef18516287921f
Yonis123 <ynur472@gmail.com>
2024-07-18 12:13:58 -0400

    this is the raw style chang with the beige bg and black lines

--- src/components/Node.js
+++ src/components/Node.js
@@ -9,27 +9,40 @@ class Node extends React.Component {
   render() {
     const { position, controllingPlayer, className } = this.props;
     const playerStyles = [
-      { width: 20, height: 20, bkg: "#D3D3D3" }, // Empty node
+      { width: 10, height: 10, bkg: "#333333" }, // Empty node
       { width: 30, height: 30, bkg: "#CCEDEF" }, // Player 1
       { width: 30, height: 30, bkg: "#2F4F4F" }  // Player 2
     ];
     
     const playerStyle = playerStyles[controllingPlayer] || playerStyles[0];
     
+    const hitboxSize = 30; // Increase hitbox size to 40x40px
     const style = {
       left: `${position.xPos}%`,
       top: `${position.yPos}%`,
+      width: `${hitboxSize}px`,
+      height: `${hitboxSize}px`,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      transform: 'translate(-50%, -50%)',
+    };
+
+    const nodeStyle = {
       width: `${playerStyle.width}px`,
       height: `${playerStyle.height}px`,
       backgroundColor: playerStyle.bkg,
+      borderRadius: '50%',
     };
 
     return (
       <div 
         className={`node ${className}`} 
         style={style} 
-        onClick={this.onNodeClick} 
-      />
+        onClick={this.onNodeClick}
+      >
+        <div style={nodeStyle} />
+      </div>
     );
   }
 }
